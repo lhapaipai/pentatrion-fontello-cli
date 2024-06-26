4dc3201f3a993893acc31ea68185e868485e771b
Hugues Tavernier <hugues.tavernier@protonmail.com>
2024-06-26 17:45:33 +0200

    remove bin/dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-type IconSetConfig = {
-    name: string;
-    base: string;
-};
-type ProjectConfig = IconSetConfig | IconSetConfig[];
-
-declare function defineConfig(config: ProjectConfig): ProjectConfig;
-
-export { defineConfig };
